6eeb3611a8a0986c530afab3df72cfb152449cf7
blyncnov <bholuwatife00@gmail.com>
2023-05-14 19:43:06 +0100

    TypeORM module fix

--- backend/src/Entities/User.ts
+++ backend/src/Entities/User.ts
@@ -1,1 +1,13 @@
-export const Ayo = "";
+// import { Entity, BaseEntity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+// @Entity("user")
+// export class User extends BaseEntity {
+//   @PrimaryGeneratedColumn()
+//   id: number;
+
+//   @Column()
+//   Name: string;
+
+//   @Column()
+//   age: number;
+// }
