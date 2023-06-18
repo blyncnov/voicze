d9bc25f2d48156dc2bdeb0e3157a0b944a33c48d
blyncnov <bholuwatife00@gmail.com>
2023-06-18 13:20:52 +0100

    voicze => adjustment

--- axios.config.ts
+++ axios.config.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from "axios";
 
 export const voicze_config: AxiosInstance = axios.create({
-  baseURL: "https://voicze-api.onrender.com/api/v1",
+  baseURL: process.env.BASE_URL,
   timeout: 3000,
   headers: {
     "Content-Type": "application/json",
