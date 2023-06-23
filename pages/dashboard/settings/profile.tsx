2bb9d78a38e492f2d8ec334b413c845e1de2e956
blyncnov <bholuwatife00@gmail.com>
2023-06-24 00:06:13 +0100

    voicze => Profile settings page

--- pages/dashboard/settings/profile.tsx
+++ pages/dashboard/settings/profile.tsx
@@ -48,7 +48,7 @@ const Profile = () => {
             </section>
           </div>
           <div className="profile_main_section">
-            <ProfileSettings />
+            <InvoiceSettings />
           </div>
         </div>
       </div>
@@ -67,11 +67,11 @@ const ProfileSettings = () => {
         <div id="form_grid_container">
           <div id="grouped_layer">
             <div className="single_column">
-              <label htmlFor="first_name">First Name</label>
+              <label htmlFor="business_name">First Name</label>
               <input
                 type="text"
-                id="first_name"
-                name="first_name"
+                id="business_name"
+                name="business_name"
                 defaultValue="Helen_dee "
               />
             </div>
@@ -127,6 +127,16 @@ const ProfileSettings = () => {
           </div>
           <div id="grouped_layer">
             <div className="single_column">
+              <label htmlFor="state">Home Address</label>
+              <textarea
+                id="business_address"
+                name="business_address"
+                defaultValue="Harmony Estate,Alabata, Ogun State "
+              />
+            </div>
+          </div>
+          <div id="grouped_layer">
+            <div className="single_column">
               <label htmlFor="city_name">City</label>
               <input
                 type="text"
@@ -147,7 +157,6 @@ const ProfileSettings = () => {
           </div>
         </div>
         <br />
-        <br />
         <div className="submit_button">
           <button type="submit">Update Account</button>
         </div>
@@ -159,7 +168,102 @@ const ProfileSettings = () => {
 const BusinessSettings = () => {
   return (
     <div>
-      <h3>business profile</h3>
+      <h3>Business Settings</h3>
+      <br />
+      <form method="post">
+        <div id="form_grid_container">
+          <div id="grouped_layer">
+            <div className="single_column">
+              <label htmlFor="business_name">Business Name</label>
+              <input
+                type="text"
+                id="business_name"
+                name="business_name"
+                defaultValue="Helena's Inc. "
+              />
+            </div>
+          </div>
+          <div id="grouped_layer">
+            <div className="single_column">
+              <label htmlFor="phone_number">Business category</label>
+              <select name="category" id="category">
+                <option value="Individual/Freelancer">
+                  Individual/Freelancer
+                </option>
+                <option value="Health">Health Sector</option>
+                <option value="Iyaloja">Iyaloja</option>
+              </select>
+            </div>
+          </div>
+          <div id="grouped_layer">
+            <div className="single_column">
+              <label htmlFor="business_email_address">
+                Business Email Address
+              </label>
+              <input
+                type="text"
+                id="business_email_address"
+                name="business_email_address"
+                defaultValue=" Helen_dee001@voicze.io"
+              />
+            </div>
+          </div>
+          <div id="grouped_layer">
+            <div className="single_column">
+              <label htmlFor="state">Business Address</label>
+              <textarea
+                id="business_address"
+                name="business_address"
+                defaultValue="113 close, Hugary main 5yt, Funaab, Alabata, Ogun State "
+              />
+            </div>
+          </div>
+          <div id="grouped_layer">
+            <div className="single_column">
+              <label htmlFor="state">State/Province</label>
+              <input
+                type="text"
+                id="state"
+                name="state"
+                defaultValue="Ogun State "
+              />
+            </div>
+            <div className="single_column">
+              <label htmlFor="postcode">Postcode</label>
+              <input
+                type="text"
+                id="postcode"
+                name="postcode"
+                defaultValue="120201"
+              />
+            </div>
+          </div>
+          <div id="grouped_layer">
+            <div className="single_column">
+              <label htmlFor="city_name">City</label>
+              <input
+                type="text"
+                id="city_name"
+                name="city_name"
+                defaultValue=" Abeokuta"
+              />
+            </div>
+            <div className="single_column">
+              <label htmlFor="country">Country </label>
+              <input
+                type="text"
+                id="country"
+                name="country"
+                defaultValue="Nigeria "
+              />
+            </div>
+          </div>
+        </div>
+        <br />
+        <div className="submit_button">
+          <button type="submit">Update Account</button>
+        </div>
+      </form>
     </div>
   );
 };
@@ -167,7 +271,73 @@ const BusinessSettings = () => {
 const InvoiceSettings = () => {
   return (
     <div>
-      <h3>Invoice profile</h3>
+      <h3>Invoice Settings</h3>
+      <br />
+      <form method="post">
+        <div id="form_grid_container">
+          <div id="grouped_layer">
+            <div className="single_column">
+              <label htmlFor="currency_iso">Currency </label>
+              <select name="currency_iso" id="currency_iso">
+                <option value="Nigerian Naira.">Nigerian Naira</option>
+                <option value="usd">USD</option>
+                <option value="euro">Euro</option>
+              </select>
+            </div>
+          </div>
+          <div id="grouped_layer">
+            <div className="single_column">
+              <label htmlFor="tax_rate">Tax Rate (%)</label>
+              <input
+                type="number"
+                id="tax_rate"
+                name="tax_rate"
+                defaultValue={0}
+              />
+            </div>
+          </div>
+          <div id="grouped_layer">
+            <div className="single_column">
+              <label htmlFor="invoice_prefix">Invoice Prefix</label>
+              <input
+                type="text"
+                id="invoice_prefix"
+                name="invoice_prefix"
+                defaultValue="voicze"
+              />
+            </div>
+          </div>
+          <div id="grouped_layer">
+            <div className="single_column">
+              <label htmlFor="auto_reminder">Auto Reminder</label>
+              <select name="auto_reminder" id="auto_reminder">
+                <option value="Enabled">Enabled</option>
+                <option value="Disable">Disable</option>
+              </select>
+            </div>
+          </div>
+          <div id="grouped_layer">
+            <div className="single_column">
+              <label htmlFor="sms_notification">SMS Notification</label>
+              <select name="sms_notification" id="sms_notification">
+                <option value="Enabled">Enabled</option>
+                <option value="Disable">Disable</option>
+              </select>
+            </div>
+            <div className="single_column">
+              <label htmlFor="auto_bill">Auto PayBill</label>
+              <select name="auto_bill" id="auto_bill">
+                <option value="Enabled">Enabled</option>
+                <option value="Disable">Disable</option>
+              </select>
+            </div>
+          </div>
+        </div>
+        <br />
+        <div className="submit_button">
+          <button type="submit">Update Account</button>
+        </div>
+      </form>
     </div>
   );
 };
