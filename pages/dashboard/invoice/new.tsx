3e6e702e2244dae29f7e5b1c8ca20c670287335d
blyncnov <bholuwatife00@gmail.com>
2023-06-26 03:19:53 +0100

    voicze => color update v1.1.2

--- pages/dashboard/invoice/new.tsx
+++ pages/dashboard/invoice/new.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 
+// Soon Components
+import ComingSoon from "@/components/Soon";
+
 const CreateNewInvoice = () => {
   return (
     <div>
-      <h1>Create New Invoice</h1>
+      <ComingSoon />
     </div>
   );
 };
