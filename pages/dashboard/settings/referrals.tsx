4b957b1ec1c9427a3131b7c58c045aabb4d67e30
blyncnov <bholuwatife00@gmail.com>
2023-06-26 02:52:32 +0100

    voicze => referal page 1.1.2

--- pages/dashboard/settings/referrals.tsx
+++ pages/dashboard/settings/referrals.tsx
@@ -10,11 +10,11 @@ import { refLevelType, refType } from "@/types/types";
 
 // Icon Style
 const IconS = {
-  fontSize: "4em",
+  fontSize: "3em",
   color: "#327be8",
 };
 
-const Referals: refType[] = [
+const Referral: refType[] = [
   {
     id: 1,
     title: "Referrals",
@@ -32,7 +32,7 @@ const Referals: refType[] = [
   },
 ];
 
-const ReferalsLevels: refLevelType[] = [
+const ReferralLevels: refLevelType[] = [
   {
     id: 1,
     title: "Referral Level",
@@ -50,13 +50,13 @@ const ReferalsLevels: refLevelType[] = [
 const Referrals = () => {
   return (
     <div id="special_shadow_container">
-      <h3 className="referals_title">Referrals</h3>
+      <h3 className="referral_title">Referrals</h3>
       <br />
-      <section id="referals_options">
-        <div className="referals_grid_container">
-          {Referals.map((data, idx) => {
+      <section id="referral_options">
+        <div className="referral_grid_container">
+          {Referral.map((data, idx) => {
             return (
-              <div className="referals_choose_options" key={idx}>
+              <div className="referral_choose_options" key={idx}>
                 <div>{data.icon}</div>
                 <p>{data.title} </p>
               </div>
@@ -65,8 +65,8 @@ const Referrals = () => {
         </div>
       </section>
       <br />
-      <section id="referals_desc">
-        <h2 className="referals_title">Referals </h2>
+      <section id="referral_desc">
+        <h2 className="referral_title">Referral Instruction</h2>
         <p>
           Submit your promo code, send points with your friends or donate to
           chat pool.
@@ -74,14 +74,14 @@ const Referrals = () => {
       </section>
       <br />
       <br />
-      <section id="referals_full_details">
-        <div className="referals_details_grid">
-          <div className="referals_column">
+      <section id="referral_full_details">
+        <div className="referral_details_grid">
+          <div className="referral_column">
             <div>
-              <h3 className="referals_title"> Your Referals Code</h3>
+              <h3 className="referral_title"> Your referral Code</h3>
             </div>
-            <div className="referals_code">
-              <div className="referals_code_value">
+            <div className="referral_code">
+              <div className="referral_code_value">
                 <p> Blyncnov_key000000777 </p>
               </div>
               <div className="referal_copy_button">
@@ -89,14 +89,14 @@ const Referrals = () => {
               </div>
             </div>
           </div>
-          <div className="referals_column">
-            {ReferalsLevels.map((data, idx) => {
+          <div className="referral_column">
+            {ReferralLevels.map((data, idx) => {
               return (
-                <div key={idx} className="referals_select">
+                <div key={idx} className="referral_select">
                   <div>
-                    <h3 className="referals_title"> {data.title} </h3>
+                    <h3 className="referral_title"> {data.title} </h3>
                   </div>
-                  <div className="referals_select_options">
+                  <div className="referral_select_options">
                     <p>Level #{data.id} </p>
                   </div>
                 </div>
